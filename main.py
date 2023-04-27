8689775c08622fb6448e2b20a272204213180453
Carlos N. Trejo <14352052+CarlosTrejo2308@users.noreply.github.com>
2023-04-26 19:44:34 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import random
+import enum
+
+class Evento(enum):
+    Llegada = 1
+    Salida = 2
+
+class listaEventos:
+    def obtenerEventoInminente(self):
+        pass
+
+class simulacion():
+    def __init__(self, semilla) -> None:
+        self.tiempoMedioEntreLlegadas = 4.5
+        self.tiempoMedioServicio = 3.5
+        self.SIGMA = 0.6
+        self.clientesTotales = 100
+
+        random = random.Random(semilla)
+        self.listaDeEventos = listaEventos()
+        self.clientes = []
+
+        self.inicializar()
+
+
+    def inicializar(self):
+        pass
+
+    def run(self):
+        while(self.numeroDeSalidas < self.clientesTotales):
+            evento = self.listaDeEventos.obtenerEventoInminente()
+            reloj = evento.obtenerTiempo()
+
+            if(evento.obtenerTipo() == Evento.Llegada):
+                self.procesoLlegada()
+            else:
+                self.procesoSalida()
+
+        self.generarReporte()
+
+
+    def inicializar():
+        pass
+
+    def procesoLlegada(self):
+        pass
+
+    def procesoSalida(self):
+        pass
+
+    def generarReporte(self):
+        pass
